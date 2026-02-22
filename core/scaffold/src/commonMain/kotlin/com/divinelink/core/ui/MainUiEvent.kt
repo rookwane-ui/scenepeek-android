f185a89af2280fffd437f0af5e6352c9db121e68
rookwane-ui <rookwane@gmail.com>
2026-02-22 21:26:33 +0200

    Update MainUiEvent.kt

--- core/scaffold/src/commonMain/kotlin/com/divinelink/core/ui/MainUiEvent.kt
+++ core/scaffold/src/commonMain/kotlin/com/divinelink/core/ui/MainUiEvent.kt
@@ -6,6 +6,4 @@ sealed interface MainUiEvent {
   data object None : MainUiEvent
   data class NavigateToDetails(val route: Navigation.DetailsRoute) : MainUiEvent
   data class NavigateToPersonDetails(val route: Navigation.PersonRoute) : MainUiEvent
-  // ✅ ضيف الحدث الجديد هنا
-  data class NavigateToVidsrcPlayer(val mediaId: Int, val mediaType: String) : MainUiEvent
 }
